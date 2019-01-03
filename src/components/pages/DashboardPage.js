7690c0a39423ae16213aee4a358942e671eae4c9
rickithadi <rickithadi@gmail.com>
2019-01-03 14:20:22 +0800

    listing titles

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -11,13 +11,26 @@ class DashboardPage extends React.Component {
 
   onInit = props => props.fetchBooks();
 
+    // ListBooks(books){
+    //     console.log('receiving', books);
+    //     this.books.map(function(book){
+    //     return <li key={book._id}>{book.title}</li> 
+    //     });
+    // }
   render() {
     const { isConfirmed, books } = this.props;
-    return (
+
+   return (
       <div>
         {!isConfirmed && <ConfirmEmailMessage />}
 
-        {books.length === 0 ? <AddBookCtA /> : <p>You have books!</p>}
+        {/* {books.length === 0 ? <AddBookCtA /> : <p>You have books!</p>} */}
+        {books.length === 0 ? <AddBookCtA /> : <ul className="bookList">
+                                                 {this.props.books.map(function(book){
+                                                     return <li key={book._id}>{book.title}</li>
+                                                 })
+                                                 } 
+                                               </ul>} 
       </div>
     );
   }
