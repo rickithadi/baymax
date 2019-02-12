9850cede2fb9e140864daef02d21a777e3e1e8d1
rickithadi <rickithadi@gmail.com>
2019-02-12 08:24:53 +0800

    added charts and calling the simplegraph comp from dash. Gonna nap

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import ResetPasswordPage from "./components/pages/ResetPasswordPage";
 import NewBookPage from "./components/pages/NewBookPage";
 import NewWorkoutPage from "./components/pages/NewWorkoutPage";
-import UserSettingsPage from "./components/pages/UserSettingsPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
 import TopNavigation from "./components/navigation/TopNavigation";
@@ -51,13 +50,7 @@ const App = ({ location, isAuthenticated }) => (
       exact
       component={DashboardPage}
     />
-    <UserRoute
-      location={location}
-      path="/settings"
-      exact
-      component={UserSettingsPage}
-    />
-    {/* todo add ex component */}
+   {/* todo add ex component */}
     <UserRoute
       location={location}
       path="/workouts/new"
--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Icon} from "semantic-ui-react"
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import ConfirmEmailMessage from "../messages/ConfirmEmailMessage";
@@ -9,6 +10,8 @@ import { fetchWorkouts } from "../../actions/workouts";
 import { fetchExercises } from "../../actions/exercises";
 import Moment from 'react-moment';
 import {Card, Grid, Header} from 'semantic-ui-react';
+import SimpleGraph from "../forms/SimpleGraph";
+
 class DashboardPage extends React.Component {
   componentDidMount = () => this.onInit(this.props);
 
@@ -38,27 +41,29 @@ class DashboardPage extends React.Component {
        {workouts.length === 0 ? <AddWorkoutCtA /> : <div className="bookList">
                                                       {this.props.workouts.slice(0).reverse().map((workout)=>{
                                                    return <Card key={workout._id} fluid >
-                                                            <Card.Content>
-                                                              <Card.Header style={{display: 'flex'}}>
-                                                                <Moment format="D MMM YYYY">{workout.date}</Moment>
-                                                              </Card.Header>
-                                                              <Card.Meta style={{display: 'flex'}}>
-                                                                <p>
-                                                                  <span style={{marginTop: '30px'}}>{workout.desc}</span>
-                                                                </p>
-                                                              </Card.Meta>
-                                                            </Card.Content>
-                                                            <Card.Content extra>
-                                                              <Card.Group stackable itemsPerRow={4}>
-                                                                    {this.props.exercises.map((exercise) => {
-                                                                        if (exercise.workoutId===workout._id){
-                                                                          return (
-                                                                            <Card key={exercise._id}>
-                                                                              <Card.Header textAlign='center'><h3 >{exercise.name}</h3></Card.Header>
-                                                                              <Card.Content textAlign='center'>
-                                                                                <Card.Header textAlign='center' style={{color:'lightblue'}}>{exercise.weight}kg</Card.Header>
+    <Card.Content>
+      <Card.Header style={{display: 'flex'}}>
+        <Moment format="D MMM YYYY">{workout.date}</Moment>
+      </Card.Header>
+      <Card.Meta style={{display: 'flex'}}>
+        <p>
+          <span style={{marginTop: '30px'}}>{workout.desc}</span>
+        </p>
+      </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <Card.Group stackable itemsPerRow={4}>
+            {this.props.exercises.map((exercise) => {
+                if (exercise.workoutId===workout._id){
+                  return (
+                    <Card key={exercise._id}>
+                      <Card.Header textAlign='center'><h3 >{exercise.name}</h3></Card.Header>
+<Card.Content textAlign='center'>
+<Card.Header textAlign='center' style={{color:'lightblue'}}>{exercise.weight}kg</Card.Header>
                                                                                 {exercise.sets} sets X {exercise.reps} reps
-                                                                              </Card.Content>
+                                                                                </Card.Content>
+					     <Icon name="line graph" size="big"/>
+	      <SimpleGraph exercise={exercise}/>
                                                                               </Card>
                                                                           )
                                                                                  }
