860f88a4a4abc228477aaa71ddb9c1dd856bd658
rickithadi <rickithadi@gmail.com>
2019-02-13 10:17:41 +0800

    line rendering in modal. Need to plug in exercises and implement get call

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -10,6 +10,7 @@ import { fetchWorkouts } from "../../actions/workouts";
 import { fetchExercises } from "../../actions/exercises";
 import Moment from 'react-moment';
 import {Card,Button, Grid, Header} from 'semantic-ui-react';
+import {Line,AreaChart, LineChart,Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
 
 class DashboardPage extends React.Component {
 state={
@@ -31,7 +32,7 @@ temp_ex:{}
 
     filterExercises= (list,w_id) => {
         list.map(function(exercise){
-            if(exercise.workoutId==w_id){
+            if(exercise.workoutId===w_id){
                 return <b> yup {exercise.w_id}</b>;
 
             }
@@ -48,6 +49,21 @@ return (
 	}
    render() {
      const { isConfirmed, workouts, exercises,filterExercises} = this.props;
+  const data = [
+           {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+         {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+       {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+            {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+           {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+	         {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+       {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
+					       ];
+     const renderLineChart = (
+       <LineChart width={400} height={400} data={data}>
+           <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+	     </LineChart>
+	     );
+
        let restable = this.props.exercises.map((exercise) => {
            return exercise.name;
            /// ...
@@ -56,9 +72,10 @@ return (
       <div>
         {!isConfirmed && <ConfirmEmailMessage />}
 
+	{renderLineChart}
        {workouts.length === 0 ? <AddWorkoutCtA /> : <div className="bookList">
-                                                      {this.props.workouts.slice(0).reverse().map((workout)=>{
-                                                   return <Card key={workout._id} fluid >
+         {this.props.workouts.slice(0).reverse().map((workout)=>{
+                 return <Card key={workout._id} fluid >
     <Card.Content>
       <Card.Header style={{display: 'flex'}}>
         <Moment format="D MMM YYYY">{workout.date}</Moment>
@@ -79,8 +96,7 @@ return (
 <Card.Content textAlign='center'>
 <Card.Header textAlign='center' style={{color:'red'}}>{exercise.weight}kg</Card.Header>
                                                                                 {exercise.sets} sets X {exercise.reps} reps
-
-                                                                       </Card.Content>
+                                        </Card.Content>
                                         <Card.Content extra>
                                             <div className='ui two buttons'>
 <Button onClick={()=>this.handleOpen(exercise)}>							     <Icon name="line graph" size="large"/>
@@ -109,6 +125,16 @@ return (
                         <Modal.Content>
 				{this.state.temp_ex.sets}X
 	{this.state.temp_ex.reps}
+
+	{renderLineChart}
+						        <LineChart data={data}
+							              margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+								          <CartesianGrid strokeDasharray="3 3"/>
+									            <XAxis dataKey="name"/>
+										              <YAxis/>
+											                <Tooltip/>
+	   <Line type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+      </LineChart>
 		</Modal.Content>
 
 	      </Modal>
