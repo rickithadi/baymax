6c5f7dd4c7da90681a83026ebca366e092db2a19
rickithadi <rickithadi@gmail.com>
2019-02-14 03:50:51 +0800

    chart is responsive

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -10,7 +10,7 @@ import { fetchWorkouts } from "../../actions/workouts";
 import { fetchExercises } from "../../actions/exercises";
 import Moment from 'react-moment';
 import {Card,Button, Grid, Header} from 'semantic-ui-react';
-import {Line,AreaChart, LineChart,Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
+import {Line,Legend,AreaChart, LineChart,Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
 
 class DashboardPage extends React.Component {
 state={
@@ -40,29 +40,32 @@ temp_ex:{}
         });
                         }
 
-	generateGraph=(exercise)=>{
-console.log(' generating with', exercise);
-return (
-<div>
-      </div>
-      )
-	}
    render() {
      const { isConfirmed, workouts, exercises,filterExercises} = this.props;
   const data = [
-           {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-         {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+       {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
        {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-            {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-           {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-	         {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+       {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+       {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+       {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
        {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
-					       ];
-     const renderLineChart = (
-       <LineChart width={400} height={400} data={data}>
+	       ];
+     const renderLineChart = (data)=>{
+     return (
+	     <ResponsiveContainer width={"100%"} height="80%">
+       <LineChart width={600} height={400} data={data}>
            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+	   <XAxis dataKey="name"/>
+          <YAxis/>
+         <CartesianGrid strokeDasharray="3 3"/>
+	        <Tooltip/>
+	       <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+             <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
 	     </LineChart>
-	     );
+	      </ResponsiveContainer>
+	     );}
 
        let restable = this.props.exercises.map((exercise) => {
            return exercise.name;
@@ -72,7 +75,6 @@ return (
       <div>
         {!isConfirmed && <ConfirmEmailMessage />}
 
-	{renderLineChart}
        {workouts.length === 0 ? <AddWorkoutCtA /> : <div className="bookList">
          {this.props.workouts.slice(0).reverse().map((workout)=>{
                  return <Card key={workout._id} fluid >
@@ -99,10 +101,13 @@ return (
                                         </Card.Content>
                                         <Card.Content extra>
                                             <div className='ui two buttons'>
-<Button onClick={()=>this.handleOpen(exercise)}>							     <Icon name="line graph" size="large"/>
-                                     </Button>
-<Button onClick={()=>this.setState({penis:exercise})}>							     <Icon name="info" size="large"/>
-                                     </Button>
+	<Button onClick={()=>this.handleOpen(exercise)}>
+	<Icon name="line graph" size="large"/>
+     </Button>
+
+     <Button onClick={()=>this.setState({penis:exercise})}>
+     <Icon name="info" size="large"/>
+     </Button>
 </div>
                                         </Card.Content>
 
@@ -115,27 +120,16 @@ return (
                                                  })
                                                  }
                                                </div>}
-		      <Modal
-	              open={this.state.modalOpen}
-       	            size='large'
-                    onClose={this.handleClose}
-		    dimmer='blurring'
-                    >
-                        <Header icon='browser' content=	{this.state.temp_ex.name}/>
-                        <Modal.Content>
-				{this.state.temp_ex.sets}X
-	{this.state.temp_ex.reps}
-
-	{renderLineChart}
-						        <LineChart data={data}
-							              margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-								          <CartesianGrid strokeDasharray="3 3"/>
-									            <XAxis dataKey="name"/>
-										              <YAxis/>
-											                <Tooltip/>
-	   <Line type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
-      </LineChart>
-		</Modal.Content>
+	      <Modal
+              open={this.state.modalOpen}
+            size='large'
+            onClose={this.handleClose}
+	    dimmer='blurring'
+                >
+                    <Header icon='eye' content={this.state.temp_ex.name}/>
+                    <Modal.Content style={{height:'100vh'}}>
+		    {renderLineChart(data)}
+	</Modal.Content>
 
 	      </Modal>
 
